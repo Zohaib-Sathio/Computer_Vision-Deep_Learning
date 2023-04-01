68457fd456131648b693c60a07c1293397a6e730
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2023-04-01 19:47:02 +0500

    added classNames and displaying confidence while detection

--- YOLO with Webcam/YOLO_with_Webcam.py
+++ YOLO with Webcam/YOLO_with_Webcam.py
@@ -10,6 +10,26 @@ cap.set(4, 480)
 
 model = YOLO('../YOLO_Weights/yolov8n.pt')
 
+classNames = ['person', 'bicycle', 'car', 'motorcycle',
+              'airplane','bus','train','truck',   'boat', 'traffic light',
+              'fire hydrant', 'stop sign',
+              'parking meter','bench', 'bird','cat',
+              'dog','horse','sheep',  'cow',  'elephant',  'bear',   'zebra',
+              'giraffe',   'backpack',
+              'umbrella',   'handbag', 'tie',  'suitcase',  'frisbee',
+              'skis',  'snowboard',   'sports ball',  'kite',
+              'baseball bat',   'baseball glove',
+              'skateboard', 'surfboard', 'tennis racket',  'bottle',
+              'wine glass', 'cup',  'fork','knife',
+              'spoon',   'bowl', 'banana',  'apple', 'sandwich',  'orange',
+              'broccoli', 'carrot',  'hot dog', 'pizza',
+              'donut',  'cake', 'chair',  'couch',
+              'potted plant',  'bed',  'dining table',
+              'toilet', 'tv','laptop', 'mouse', 'remote',  'keyboard',
+              'cell phone',  'microwave',  'oven',  'toaster',   'sink',
+              'refrigerator',   'book', 'clock', 'vase',
+              'scissors',  'teddy bear', 'hair drier', 'toothbrush']
+
 fTime = 0
 cTime = 0
 
@@ -20,14 +40,18 @@ while True:
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
-            cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (233, 50, 10), 3)
-
-            cvzone.putTextRect(img, str(math.ceil(box.conf[0]*100)/100),(100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255))
+            cvzone.cornerRect(img, (int(x1), int(y1), int(x2) - int(x1), int(y2) - int(y1)))
+            # cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (233, 50, 10), 3)
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # print(conf)
+            cvzone.putTextRect(img, f'{conf}', (max(20, int(x1)),max(50, int(y1))), 2, 2)
+            # cvzone.putTextRect(img, str(conf), (max(0, x1), max(35, y1)))
 
     cTime = time.time()
     fps = 1 / (cTime - fTime)
     fTime = cTime
-    cv2.putText(img, str(int(fps)), (40, 60), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 0), 1)
+    # print(fps)
+    cv2.putText(img, str(int(fps)), (40, 60), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (255, 0, 0), 1)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
